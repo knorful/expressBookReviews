a94c153954676706e82d028631dae9a6c553e3ba
knorful <krisnorful@gmail.com>
2024-06-16 14:52:19 -0500

    get book details by title and author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,27 +11,39 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  //Write your code here
   res.send(books);
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
   let isbn = req.params.isbn;
   res.send(books[isbn]);
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let author = req.params.author;
+  let filtered_book;
+  for (let book in books) {
+    let booksObj = books[book];
+    if (booksObj.author === author) {
+      filtered_book = booksObj;
+    }
+  }
+  res.send(filtered_book);
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let title = req.params.title;
+  let filtered_book;
+  for (let book in books) {
+    let booksObj = books[book];
+    if (booksObj.title === title) {
+      filtered_book = booksObj;
+    }
+  }
+  res.send(filtered_book);
 });
 
 //  Get book review
